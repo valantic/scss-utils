75535cf96024effa1ad04203c211f3220e1b5894
Philip Nachbauer <hello@haba.at>
2026-02-19 14:26:50 +0100

    Enhancement: updated versions and fixed issues.

--- .stylelintrc.fix.mjs
+++ .stylelintrc.fix.mjs
@@ -1,11 +1,4 @@
 export default {
   extends: ['stylelint-config-valantic/fix', './.stylelintrc.js'],
-  overrides: [
-    {
-      files: ['src/styleguide/routes/**/*.*'],
-      rules: {
-        'selector-class-pattern': null,
-      },
-    },
-  ],
+  overrides: [],
 };
